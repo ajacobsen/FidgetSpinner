6eff1c99035253e55927dd5b5ef3d089feb07fd0
Toby Strong <toby.strong1@hotmail.co.uk>
2017-05-18 11:01:55 +0100

    Fix

---
+++ src/main/java/me/tobystrong/spinner/item/ItemSpinner.java
@@ -0,0 +1,76 @@
+package me.tobystrong.spinner.item;
+
+import me.tobystrong.spinner.FidgetSpinner;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.IItemPropertyGetter;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * Created by tobystrong on 10/05/2017.
+ */
+public class ItemSpinner extends Item
+{
+    public ItemSpinner()
+    {
+        super();
+        setRegistryName("fidget_spinner");
+        setUnlocalizedName("fidget_spinner");
+        setMaxStackSize(1);
+        setHasSubtypes(true);
+        setCreativeTab(CreativeTabs.MISC);
+
+        this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            }
+        });
+    }
+
+    @Override
+    public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        for(int i = 0; i < 16; i++)
+        {
+            subItems.add(new ItemStack(FidgetSpinner.instance.fidget_spinner, 1, i));
+        }
+    }
+
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getMaxItemUseDuration(ItemStack stack)
+    {
+        return 72000;
+    }
+
+    /**
+     * returns the action that specifies what animation to play when the items is being used
+     */
+    public EnumAction getItemUseAction(ItemStack stack)
+    {
+        return EnumAction.NONE;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack stackIn, World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        playerIn.setActiveHand(handIn);
+        playerIn.playSound(SoundEvents.UI_BUTTON_CLICK, 1f, 1f);
+        return new ActionResult(EnumActionResult.SUCCESS, stackIn);
+    }
+}
---
+++ src/main/java/me/tobystrong/spinner/proxy/ClientProxy.java
@@ -0,0 +1,53 @@
+package me.tobystrong.spinner.proxy;
+
+import me.tobystrong.spinner.FidgetSpinner;
+import me.tobystrong.spinner.ModelUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+
+/**
+ * Created by tobystrong on 10/05/2017.
+ */
+public class ClientProxy implements IProxy
+{
+    private static final int[] colors = new int[]{
+        0xE9ECEC,
+        0xF07613,
+        0xBD44B3,
+        0x3AAFD9,
+        0xF8C627,
+        0x70B919,
+        0xED8DAC,
+        0x555d60,
+        0x8E8E86,
+        0x158991,
+        0x792AAC,
+        0x35399D,
+        0x724728,
+        0x546D1B,
+        0xA12722,
+        0x3E4447
+    };
+
+    @Override
+    public void registerRenderers()
+    {
+        for(int i = 0; i < 16; i++)
+        {
+            ModelUtil.registerItem(FidgetSpinner.instance.fidget_spinner, i, "fidgetspinner:fidget_spinner");
+        }
+
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new IItemColor() {
+            @Override
+            public int getColorFromItemstack(ItemStack stack, int tintIndex)
+            {
+                return colors[stack.getMetadata() < 16 ? stack.getMetadata() : 0];
+            }
+        }, FidgetSpinner.instance.fidget_spinner);
+    }
+}
